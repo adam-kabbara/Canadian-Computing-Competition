aee1e7964a828b200d1ae9e095a9a20e48d7a410
adam-kabbara <62020687+adam-kabbara@users.noreply.github.com>
2022-09-20 19:08:55 -0400

    added 2020 j1 java

---
+++ 2020 junior/j1 - dog treats/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+        int m = sc.nextInt();
+        int l = sc.nextInt();
+        sc.close();
+
+        if ((s + 2*m + 3*l) >= 10){
+            System.out.println("happy");
+        }
+        else{
+            System.out.println("sad");
+        }
+    }
+}
