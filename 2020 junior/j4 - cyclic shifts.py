0819d51c5f967330ddd5ea79ca80c8a77a867ba4
adam-kabbara <62020687+adam-kabbara@users.noreply.github.com>
2022-10-02 15:23:12 -0400

    updated 2020 j4 python

--- 2020 junior/j4 - cyclic shifts.py
+++ 2020 junior/j4 - cyclic shifts.py
@@ -1,13 +1,9 @@
 text = input()
 string = input()
 
-cycles = set() # so we dont get multiple similar cycles, use sets
-
 for i in range(len(string)):
-    cycles.add(string[i:]+string[:i])
-
-for c in cycles:
-    if c in text:
+    cycle = string[i:]+string[:i]
+    if cycle in text:
         print("yes")
         break
 else: # only executed if the for loop is not broken with a break statement
