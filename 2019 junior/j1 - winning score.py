68d94c8b2aacb260495bdd5e72b405d2f0e61303
adam-kabbara <62020687+adam-kabbara@users.noreply.github.com>
2022-11-03 21:16:15 -0400

    added 2019 j1 python

---
+++ 2019 junior/j1 - winning score.py
@@ -0,0 +1,8 @@
+apples = 3*int(input()) + 2*int(input()) + int(input())
+bananas = 3*int(input()) + 2*int(input()) + int(input())
+if  apples > bananas:
+    print("A")
+elif bananas > apples:
+    print("B")
+else:
+    print("T")
